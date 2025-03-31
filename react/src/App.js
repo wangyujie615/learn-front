cb3dc7dc9ab572ff4877e7446bccbd704e3f44f5
fyaft2012 <274028037@qq.com>
2025-03-31 20:35:46 +0800

    leetcode_131

--- react/src/App.js
+++ react/src/App.js
@@ -1,14 +1,39 @@
-import React from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { decrement, increment } from "./store/counterSlice";
 function App() {
     const count = useSelector(state => state.counter.value)
     const dispatch = useDispatch()
+    const [isRunning, setIsRunning] = useState(false)
+    const [num, setNum] = useState(0)
+    useEffect(() => {
+        let intervalId;
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                setNum(num + 1)
+            }, 1000)
+        }
+        return () => {
+            if (intervalId) clearInterval(intervalId)
+        }
+    }, [isRunning, num])
+    //这样会导致无限请求问题
+    const handleClick = (p)=>{
+        setNum(num+1)
+        console.log(p)
+    }
+    useEffect(()=>{
+       handleClick('pp');
+    },[handleClick])
     return (
         <div>
             <button onClick={() => dispatch(increment())}>Increment</button>
             <span>{count}</span>
             <button onClick={() => dispatch(decrement())}>decrement</button>
+            <button onClick={() => setIsRunning(!isRunning)}>start</button>
+            <span>{num}</span>
+            <button>change</button>
+            <span>{num}</span>
         </div>
     )
 }
---
+++ 算法/回溯/划分型回溯/leetcode_131.js
@@ -0,0 +1,71 @@
+/**
+ * Leetcode_131:分割回文串
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function (s) {
+    const n = s.length
+    const res = [], path = []
+    const isHuiWen = (s, start, end) => {
+        while (start < end) {
+            if (s[start] !== s[end]) return false
+            start++;
+            end--;
+        }
+        return true
+    }
+    /**
+     * @param {*} i 
+     * @param {*} start 下一个子串开始的位置 
+     * @returns 
+     */
+    const dfs = (i, start) => {
+        if (i === n) {
+            res.push(path.slice())
+            return
+        }
+        //不选i和i+1之间的逗号 不选的区间[0,n-2]
+        if (i < n - 1) {
+            dfs(i + 1, start)
+        }
+        //判断子串是否是回文
+        if (isHuiWen(s, start, i)) {
+            path.push(s.substring(start, i + 1))
+            dfs(i + 1, i + 1)
+            path.pop()
+        }
+    }
+    dfs(0, 0)
+    return res
+};
+
+var partition2 = function (s) {
+    //从答案的角度出发
+    //枚举回文子串的结束的位置
+    const n = s.length
+    const path = [], res = []
+    const isHuiWen = (s, start, end) => {
+        while (start < end) {
+            while (s[start] !== s[end]) return false
+            start++
+            end--
+        }
+        return true
+    }
+    const dfs = (start) => {
+        if (start === n) {
+            //枚举的位置到达最后时 在放入
+            res.push(path.slice())
+            return
+        }
+        for (let i = start; i < n; i++) {
+            if (isHuiWen(s, start, i)) {
+                path.push(s.substring(start, i + 1))
+                dfs(i + 1)
+                path.pop()
+            }
+        }
+    }
+    dfs(0)
+    return res
+};
