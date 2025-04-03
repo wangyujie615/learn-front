745376c7201be3cb41af84d5d2e68e1f1f3b384d
jiebao <77776338+wangyujie615@users.noreply.github.com>
2025-04-03 12:36:20 +0800

    Update leetcode_1593.js
    leetcode_1593

--- 算法/回溯/划分型回溯/leetcode_1593.js
+++ 算法/回溯/划分型回溯/leetcode_1593.js
@@ -22,3 +22,26 @@ var maxUniqueSplit = function (s) {
     dfs(0, [])
     return maxLength
 };
+
+var maxUniqueSplit2 = function (s) {
+    //拆分后子字符串唯一 数组长度最大
+    let maxLength = 0, n = s.length
+    const path = []
+    const dfs = (i, start) => {
+        if (i === n) {
+            maxLength = Math.max(maxLength, path.length)
+            return
+        }
+        if (i < n - 1) {
+            dfs(i + 1, start)
+        }
+        const t = s.substring(start, i + 1)
+        if (!path.includes(t)) {
+            path.push(t)
+            dfs(i + 1, i + 1)
+            path.pop()
+        }
+    }
+    dfs(0, 0)
+    return maxLength
+};
