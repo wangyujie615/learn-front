7e678d9fc34bf31a8662a387393b6370267c515c
jiebao <77776338+wangyujie615@users.noreply.github.com>
2025-04-14 21:55:19 +0800

    a
    leetcode_3376

---
+++ 算法/回溯/排列型回溯/leetcode_3376.js
@@ -0,0 +1,27 @@
+/**
+ * leetcode_3376:破解锁的最少时间
+ * @param {number[]} strength
+ * @param {number} k
+ * @return {number}
+ */
+var findMinimumTime = function (strength, k) {
+    const n = strength.length
+    const open = Array(n).fill(false)
+    let ans = Infinity
+    const dfs = (i, time) => {
+        if (i === n) {
+            ans = time
+            return
+        }
+        const x = 1 + k * i;
+        for (let j = 0; j < n; j++) {
+            if (!open[j]) {
+                open[j] = true
+                dfs(i + 1, time + Math.ceil(strength[j] / x))
+                open[j] = false
+            }
+        }
+    }
+    dfs(0, 0)
+    return ans
+}
