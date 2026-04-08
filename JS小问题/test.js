1b0b7700d361fcbe13c76cc2430a29ff776cb009
jiebao <77776338+wangyujie615@users.noreply.github.com>
2026-04-08 13:48:11 +0800

    笔记整理

--- JS小问题/test.js
+++
@@ -1,9 +0,0 @@
-function side1(arr){
-    arr[0] = arr[2]
-}
-function a(a,b,c=3){
-    c = 10
-    side1(arguments)
-    return a+b+c
-}
-console.log(a(1,1,1));
--- react/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import counterReducer from "./counterSlice";
-//创建Store
-const store = configureStore({
-    reducer: {
-        counter: counterReducer
-    }
-})
-export default store;
