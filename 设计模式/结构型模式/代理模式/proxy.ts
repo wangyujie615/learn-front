d96099bf2d0b575e62c57cf3708fe1c899bfaf52
wangyujie <2535310040@qq.com>
2026-07-29 00:43:28 +0800

    设计模式
    设计模式

---
+++ 设计模式/结构型模式/代理模式/proxy.ts
@@ -0,0 +1,54 @@
+interface Subject {
+    request(): void;
+}
+
+class RealSubject implements Subject {
+    request(): void {
+        console.log("🟢 RealSubject: Handling request.");
+    }   
+}
+
+class ProxySubject implements Subject {
+    private realSubject: Subject;
+
+    constructor(realSubject: Subject) {
+        this.realSubject = realSubject;
+        console.log("🟢 Proxy: Initialized.");
+    }
+
+    request(): void {
+        if (this.checkAccess()) {
+            this.realSubject.request();
+            this.logAccess();
+        }
+    }
+
+    private checkAccess(): boolean {
+        // Some real checks should go here.
+        console.log('Proxy: Checking access prior to firing a real request.');
+
+        return true;
+    }
+
+    private logAccess(): void {
+        console.log('Proxy: Logging the time of request.');
+    }
+}
+
+function clientCode(subject: Subject) {
+    // ...
+
+    subject.request();
+
+    // ...
+}
+
+console.log('Client: Executing the client code with a real subject:');
+const realSubject = new RealSubject();
+clientCode(realSubject);
+
+console.log('');
+
+console.log('Client: Executing the same client code with a proxy:');
+const proxy = new ProxySubject(realSubject);
+clientCode(proxy);
